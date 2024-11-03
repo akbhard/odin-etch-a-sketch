5865f49d83af8ade2ad5eba6010b63e4106b3436
Akshay <149981521+akbhard@users.noreply.github.com>
2024-11-03 00:46:29 -0400

    Finishing up the project, cleaned up the code

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 const container = document.querySelector(".container")
 
-for(let i = 1; i <= 16; i++){
-    const row = document.createElement("div")
-    row.className = "row"
-    container.appendChild(row)
-
-    for (let j=1; j<= 16;j++) {
-        const col = document.createElement("div")
-        col.className = "col"
-        row.appendChild(col)
-    }
-}
-
-const hoverElements = document.querySelectorAll(".col")
-
-hoverElements.forEach((element) => {
-    element.addEventListener("mouseover", () => {
-            let newColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
-            element.style.backgroundColor = newColor;
-    });
-});
-
-
-hoverElements.forEach((element) => {
-    element.addEventListener("mouseout", () => {
-        element.style.background = "antiquewhite";
-    });
-});
-
 const btn = document.querySelector("button")
 btn.addEventListener("click", () => {
-    let input = prompt("Enter in a number from 1-100")
+    let input = Number(prompt("Enter in a number from 1-100"))
 
-    if (input > 100){
+    console.log(input)
+
+    if (isNaN(input)) {
+        alert("Enter in a number dumbass")
+    } else if (input > 100){
         alert("You cannot go over 100")
     } else {
-        alert(`${input} is ok`)
+        const row = document.querySelectorAll(".row")
+        row.forEach((element) =>{
+            element.remove()
+        })
+
+        for(i=1;i<=input;i++){
+            const row = document.createElement("div")
+            row.className = "row"
+            container.appendChild(row)
+
+            for (let j=1; j<= input;j++) {
+                const col = document.createElement("div")
+                col.className = "col"
+                row.appendChild(col)
+            }
+        }
+        const hoverElements = document.querySelectorAll(".col")
+
+        hoverElements.forEach((element) => {
+            element.addEventListener("mouseover", () => {
+                    let newColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+                    element.style.backgroundColor = newColor;
+            });
+        });
+        
+        hoverElements.forEach((element) => {
+            element.addEventListener("mouseout", () => {
+                element.style.background = "antiquewhite";
+            });
+        });
     }
 })
+
